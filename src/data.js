242f3fe1d6359007a2f55864130199b97d020780
NachaSilva <110260545+NachaSilva@users.noreply.github.com>
2023-05-23 11:47:23 -0400

    Avance en css Cards y filter

--- src/data.js
+++ src/data.js
@@ -1,15 +1,10 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return "example";
-};
-
 export const anotherExample = () => {
   return "OMG";
 };
 
-//export var datospokemon = data.map(function (bar) {
-//  return "<li>" + bar.num + " " + bar.name + "</li>";
-//});
-
-//console.log(datospokemon[1].name);
+export const filterTipo = (data, optionsType) => {
+  const tipoFiltro = data.filter((element) => element.type.includes(optionsType));
+  return tipoFiltro;
+};
